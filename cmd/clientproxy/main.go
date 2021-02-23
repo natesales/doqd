d6cea1d3c5f14a4380869abefeb1c17dad739687
Nate Sales <nate@natesales.net>
2021-02-23 15:59:50 -0800

    clientproxy: add UDP DNS server

--- cmd/clientproxy/main.go
+++ cmd/clientproxy/main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"github.com/miekg/dns"
 	log "github.com/sirupsen/logrus"
+	"net"
+	"os"
 
 	"github.com/natesales/doq/pkg/client"
 )
@@ -13,44 +15,58 @@ var (
 	tlsInsecureSkipVerify = flag.Bool("insecureSkipVerify", false, "skip TLS certificate validation")
 	tlsCompat             = flag.Bool("tlsCompat", false, "enable TLS compatibility mode")
 	listenAddr            = flag.String("listen", "[::1]:5353", "udp listen address")
+	verbose               = flag.Bool("verbose", false, "enable debug logging")
 )
-
-var doqClient client.Client
-
-func handleDnsRequest(w dns.ResponseWriter, r *dns.Msg) {
-	m := new(dns.Msg)
-	m.SetReply(r)
-	m.Compress = false
-
-	switch r.Opcode {
-	case dns.OpcodeQuery:
-		resp, err := doqClient.SendQuery(*m)
-		if err != nil {
-			w.WriteMsg(&resp)
-		}
-	}
-
-	w.WriteMsg(&dns.Msg{})
-}
 
 func main() {
 	flag.Parse()
 
+	if *verbose {
+		log.SetLevel(log.DebugLevel)
+	}
+
 	// Create a new DoQ client
-	var err error
-	doqClient, err = client.New(*doqServer, *tlsInsecureSkipVerify, *tlsCompat)
+	doqClient, err := client.New(*doqServer, *tlsInsecureSkipVerify, *tlsCompat)
 	if err != nil {
 		log.Fatal(doqClient)
+		os.Exit(1)
 	}
 	defer doqClient.Close()
 
-	dns.HandleFunc(".", handleDnsRequest)
-
-	server := &dns.Server{Addr: *listenAddr, Net: "udp"}
-	log.Infof("starting DNS server on %s\n", *listenAddr)
-	err = server.ListenAndServe()
+	// Create the UDP DNS listener
+	log.Debug("creating UDP listener")
+	pc, err := net.ListenPacket("udp", *listenAddr)
 	if err != nil {
-		log.Fatalf("Failed to start server: %s\n ", err.Error())
+		return
 	}
-	defer server.Shutdown()
+	defer pc.Close()
+
+	log.Debugln("ready to accept connections")
+	for {
+		log.Debugln("ready to read from buffer")
+		buffer := make([]byte, 4096)
+		n, addr, err := pc.ReadFrom(buffer)
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+		log.Debugf("read %d bytes from buffer", n)
+
+		var msgIn dns.Msg
+		err = msgIn.Unpack(buffer)
+		if err != nil {
+			log.Warn(err)
+		}
+
+		log.Debugln("sending DoQ query")
+		resp, err := doqClient.SendQuery(msgIn)
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+
+		packed, err := resp.Pack()
+		pc.WriteTo(packed, addr)
+		log.Debug("finished writing")
+	}
 }
